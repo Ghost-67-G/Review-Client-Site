abfa52c81707f771662cb46669e0b23038cbd8bd
AyanNaseer <ayannaseer70065988@gmail.com>
2023-02-22 13:57:28 +0500

    It has a path  issue

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const companyRoute = require('./routes/company')
 const searchRoute = require('./routes/search')
 const reviewRoute = require('./routes/review')
 app.use(myExpress.json());
+const path = require('path')
+
 
 require("./db/db")
 
