edb50e42e131597b92187b16b7a100150d927390
Ghost-67-G <ayannaseer70065988@gmail.com>
2023-03-09 19:02:16 +0500

    Make some changes

--- routes/search.js
+++ routes/search.js
@@ -1,18 +1,22 @@
 const route = require("express").Router();
 const Company = require("../db/models/company");
 
-route.get('/get-search-companies', async(req,resp)=>{
-    let companies = await Company.find()
-    let searchCompanies = companies.filter((company)=>{
-        let catagories = company.company_category[0].split(',')
-        for(let catagory of catagories){
-            if(req.query.word.toLowerCase()==catagory.toLowerCase()){
-                return company
-            }
+route.get("/get-search-companies", async (req, resp) => {
+  let companies = await Company.find();
+  let searchCompanies = companies.filter((company) => {
+    let catagories = company.company_category[0].split(",");
+    if (req.query.word.toLowerCase() == company.company_name.toLowerCase()) {
+      return company;
+    } else {
+      for (let catagory of catagories) {
+        if (req.query.word.toLowerCase() == catagory.toLowerCase()) {
+          return company;
         }
-    })
+      }
+    }
+  });
 
-    resp.json({success:true,searchCompanies})
-})
+  resp.json({ success: true, searchCompanies });
+});
 
 module.exports = route;
